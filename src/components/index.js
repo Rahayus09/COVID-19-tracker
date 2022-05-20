e5053cbed80f3bf4ffca876fcf67327349f4a383
Rahayus09 <@rahayustg09@gmail.com>
2022-05-20 16:06:30 +0700

    react tracker covid-19

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Cards } from './Cards/Cards';
+export { default as ChartComp } from './ChartComp/ChartComp';
+export { default as CountryPicker } from './CountryPicker/CountryPicker';
